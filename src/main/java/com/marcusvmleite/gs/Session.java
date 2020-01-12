c7d88a89fe4f7c4595e763bfdba4d88bd703fcce
Marcus Leite <marcusvmleite@gmail.com>
2020-01-12 17:16:13 +0100

    general refact.

--- src/main/java/com/marcusvmleite/gs/Session.java
+++ src/main/java/com/marcusvmleite/gs/Session.java
@@ -11,15 +11,12 @@ import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.time.Instant;
 import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Session extends Thread {
 
     private static final Logger log = LogManager.getLogger(Session.class);
 
     private static final int TIMEOUT = 30000;
-    private static final String REGEX_UUID = "\\p{XDigit}{8}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{12}";
 
     private Socket clientSocket;
     private String sessionId;
@@ -60,24 +57,26 @@ public class Session extends Thread {
             }
 
         } catch (IOException e) {
-            log.error("An error occurred during Session [{}].", this.sessionId, e);
             if (e instanceof SocketTimeoutException) {
-                long end = Instant.now().toEpochMilli();
-                long total = end - start;
-                log.info("Session [{}] with Client [{}] finished after [{}] ms.", sessionId, clientId, total);
-                out.println(String.format(Messages.GS_004.message(), this.clientId, total));
+                log.warn("Session [{}] finished due to timeout.", this.sessionId);
+            } else {
+                log.error("An error occurred during Session [{}].", this.sessionId, e);
             }
         } finally {
-            finishSocket();
+            finishSocket(start);
         }
     }
 
-    private void finishSocket() {
+    private void finishSocket(long start) {
         try {
+            long end = Instant.now().toEpochMilli();
+            long total = end - start;
+            log.info("Session [{}] with Client [{}] finished after [{}] ms.", sessionId, clientId, total);
+            out.println(String.format(Messages.GS_004.message(), this.clientId, total));
+
             in.close();
             out.close();
             clientSocket.close();
-            log.info("Session [{}] with Client [{}] finished.", this.sessionId, this.clientId);
         } catch (IOException e) {
             log.error("An error occurred while finishing socket for Session [{}].", this.sessionId, e);
         }
@@ -96,7 +95,7 @@ public class Session extends Thread {
     }
 
     private void removeEdge(String inputLine) {
-        String[] tokens = inputLine.split(" ");
+        String[] tokens = tokenizeInput(inputLine);
         String from = tokens[2];
         String to = tokens[3];
         log.info("Removing Edge from Node [{}] to Node [{}].", from, to);
@@ -110,7 +109,7 @@ public class Session extends Thread {
     }
 
     private void removeNode(String inputLine) {
-        String[] tokens = inputLine.split(" ");
+        String[] tokens = tokenizeInput(inputLine);
         String name = tokens[2];
         log.info("Removing Node with name [{}].", name);
         if (graph.removeNode(name)) {
@@ -122,7 +121,7 @@ public class Session extends Thread {
     }
 
     private void addEdge(String inputLine) {
-        String[] tokens = inputLine.split(" ");
+        String[] tokens = tokenizeInput(inputLine);
         String from = tokens[2];
         String to = tokens[3];
         int weight = Integer.valueOf(tokens[4]);
@@ -137,7 +136,7 @@ public class Session extends Thread {
     }
 
     private void addNode(String inputLine) {
-        String[] tokens = inputLine.split(" ");
+        String[] tokens = tokenizeInput(inputLine);
         String name = tokens[2];
         log.info("Adding Node with name [{}].", name);
         if (graph.addNode(name)) {
@@ -151,24 +150,21 @@ public class Session extends Thread {
     private boolean processGreetingMessage(long start, String inputLine) {
         boolean result = true;
         if (inputLine.startsWith(Messages.GS_000.message())) {
-            Pattern pairRegex = Pattern.compile(REGEX_UUID);
-            Matcher matcher = pairRegex.matcher(inputLine);
-            if (matcher.find()) {
-                this.clientId = matcher.group(0);
-                log.info("Session [{}] received greeting from Client [{}].", this.sessionId, this.clientId);
-                out.println(String.format(Messages.GS_002.message(), this.clientId));
-            }
+            String[] tokens = tokenizeInput(inputLine);
+            this.clientId = tokens[3];
+            log.info("Session [{}] received greeting from Client [{}].", this.sessionId, this.clientId);
+            out.println(String.format(Messages.GS_002.message(), this.clientId));
         } else if (Messages.GS_003.message().equals(inputLine)) {
-            long end = Instant.now().toEpochMilli();
-            long total = end - start;
-            log.info("Session [{}] with Client [{}] finished after [{}] ms.", sessionId, clientId, total);
-            out.println(String.format(Messages.GS_004.message(), this.clientId, total));
             result = false;
         } else {
             log.warn("Session received a message that could not be recognized. Message was: [{}].", inputLine);
             out.println(Messages.GS_005.message());
         }
         return result;
+    }
+
+    private String[] tokenizeInput(String inputLine) {
+        return inputLine.split(" ");
     }
 
 }
